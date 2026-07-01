20ef8032264fad73b0fca284f28400093c5ac1a5
BrumalLake <85033680+BrumalLake@users.noreply.github.com>
2026-07-01 16:12:27 -0500

    working glfw-bindgen

---
+++ glfw-bindgen/build.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+
+fn main() {
+	// tell cargo to search this path for the shared library
+	println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+	// tell cargo to tell rustc to link libglfw
+	println!("cargo:rustc-link-lib=glfw");
+
+	let bindings = bindgen::Builder::default()
+	.header("wrapper.h")
+	// glfw flags are i32
+	.default_macro_constant_type(bindgen::MacroTypeVariation::Signed)
+	.generate()
+	.unwrap();
+
+	let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("bindings.rs");
+	bindings.write_to_file(out_path).unwrap();
+}
